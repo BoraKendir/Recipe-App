51f9f6ec63d7dcce19372d02898d3bcb1e1e7022
BoraKendir <bora.kendir@std.yeditepe.edu.tr>
2024-05-01 22:23:05 +0300

    ekrana resim basıyo console class basıyo li olarak eklenmeli

--- predict.js
+++ predict.js
@@ -1,25 +1,33 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const image = fs.readFileSync("png-clipart-tomato-tomato-thumbnail.png", {
-    encoding: "base64"
-});
+function predictImage() {
+    const image = fs.readFileSync("png-clipart-tomato-tomato-thumbnail.png", {
+        encoding: "base64"
+    });
 
-axios({
-    method: "POST",
-    url: "https://detect.roboflow.com/bitirme-abrpx/3",
-    params: {
-        api_key: "ii8ah948PuEkfwBX5ojS",
-        confidence: "0.3"
-    },
-    data: image,
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-})
-.then(function(response) {
-    console.log(response.data.predictions);
-})
-.catch(function(error) {
-    console.log(error.message);
-});
+    axios({
+        method: "POST",
+        url: "https://detect.roboflow.com/bitirme-abrpx/3",
+        params: {
+            api_key: "ii8ah948PuEkfwBX5ojS",
+            confidence: "0.3"
+        },
+        data: image,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    })
+    .then(function(response) {
+        console.log(response.data.predictions);
+        const predictions = response.data.predictions;
+
+        const uniqueClasses = [...new Set(predictions.map(prediction => prediction.class))];
+        console.log(uniqueClasses);
+    })
+    .catch(function(error) {
+        console.log(error.message);
+    });
+}
+
+predictImage();
